9d45dd0e1ba46847cd65ece0e2a142eb15926b9c
hua.wu <hua.wu@yingxiong.com>
2018-02-22 11:16:17 +0800

    docker 运行

--- d.run.sh
+++ d.run.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
+
 docker ps -a|grep gocdn |awk '{print $1}'|xargs docker kill >/dev/null 2>&1
 docker ps -a|grep gocdn |awk '{print $1}'|xargs docker rm >/dev/null 2>&1
 
-docker run \
--it \
--v ~/workspace/golang/gocdn:/go \
--v /home/wolferhua:/vhosts \
--w /go \
--p 81:8181 \
+docker run -d \
+-p 88:8181 \
 --name gocdn \
-golang:1.9.4-stretch go run src/main/main.go
-
+wolferhua/gocdn
